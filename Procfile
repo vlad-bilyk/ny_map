3ee8e0a2a4fc3947cfbbbb10c79c05a5abd9ac64
vlad-bilyk <bilyk.v@ucu.edu.ua>
2019-05-18 18:18:26 +0300

    procfile

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web gunicorn wsgi:application
+web gunicorn render:app
--- runner.py
+++ runner.py
@@ -2,9 +2,11 @@ from data import info, json_data
 from map import folium_map
 
 
-def main(day):
+def main(day, limit=1000):
     '''
     Main function including all other sub-main functions
+    :param day: str
+    :param limit: int
     :return: None
     '''
     # loading data
@@ -12,9 +14,8 @@ def main(day):
     info.get_info_with_last(day)
     print('info was collected')
     # converting data to json
-    amount = 800
     print('converting to json')
-    json_data.to_json('../NY_MAP/data/data.txt', '../NY_MAP/data/newdata.json', amount)
+    json_data.to_json('../NY_MAP/data/data.txt', '../NY_MAP/data/newdata.json', limit)
     print('converted')
     # creating a map
     print('creating a map')
